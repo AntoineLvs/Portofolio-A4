df185615ab7c52f4b1e0c9e8d044497db6b72660
AntoineLvs <antoine@elsenmedia.com>
2024-05-16 13:53:02 +0900

    nightmode + modifs

--- js/script.js
+++ js/script.js
@@ -32,16 +32,25 @@ sliders.forEach(slider => {
 
 window.addEventListener('scroll', function () {
     var header = document.getElementById('header');
+    var navLinks = document.querySelectorAll('.navlist a');
 
     if (window.scrollY > 0) {
+        navLinks.forEach(function (link) {
+            link.style.paddingBottom = '22px';
+        });
         header.style.height = '50px'; // Ajustez cette valeur pour définir la nouvelle hauteur du header lors du défilement
         header.style.opacity = '0.85';
+
     } else {
+        navLinks.forEach(function (link) {
+            link.style.paddingBottom = '39px';
+        });
         header.style.height = '100px'; // Hauteur originale du header
         header.style.opacity = '1';
 
     }
 });
+
 
 
 var buttonSavan = document.getElementById('buttonSavan');
@@ -57,26 +66,54 @@ buttonSavan.addEventListener('click', function () {
 });
 
 
-// const toggleButton = document.getElementById('toggleButton');
-// var aboutMe = document.getElementById('aboutMe');
-// var contact = document.getElementById('contact');
+var aboutMe = document.getElementById('aboutMe');
+var contact = document.getElementById('contact');
+var header = document.getElementById('header');
+var intro = document.getElementById('intro');
+var project = document.getElementById('project');
 
 
-// let isOn = false;
-
-// toggleButton.addEventListener('click', function () {
-//     isOn = !isOn; // Inverse l'état actuel (on/off)
-
-//     if (isOn) {
-
-//         aboutMe.style.backgroundColor = 'rgb(150 163 191)';
-//         contact.style.backgroundColor = 'rgb(150 163 191)';
-
-//     } else {
-
-//         aboutMe.style.backgroundColor = 'rgba(32, 41, 56, 1)';
-//         contact.style.backgroundColor = 'rgba(32, 41, 56, 1)';
 
 
-//     }
-// });
+let isOn = false;
+const toggleButton = document.getElementById('toggleButton');
+const lightElements = document.querySelectorAll('.light');
+const darkElements = document.querySelectorAll('.dark');
+const buttonElements = document.querySelectorAll('.switchButton');
+const textElements = document.querySelectorAll('.text-light');
+
+
+toggleButton.addEventListener('click', function () {
+    isOn = !isOn; // Inverse l'état actuel (on/off)
+
+    lightElements.forEach(function (element) {
+        if (isOn) {
+            element.classList.add('lightnight');
+        } else {
+            element.classList.remove('lightnight');
+        }
+    });
+
+    darkElements.forEach(function (element) {
+        if (isOn) {
+            element.classList.add('darknight');
+        } else {
+            element.classList.remove('darknight');
+        }
+    });
+
+    buttonElements.forEach(function (element) {
+        if (isOn) {
+            element.classList.add('switchButton');
+        } else {
+            element.classList.remove('switchButton');
+        }
+    });
+    textElements.forEach(function (element) {
+        if (isOn) {
+            element.classList.add('darkText');
+        } else {
+            element.classList.remove('darkText');
+        }
+    });
+});
